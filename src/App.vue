806afb2631e1ce9ca57af40b1eaa4499fcc4b1ec
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-07-18 16:58:07 +0200

    feat: complete show card flats after research

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import Header from "./components/Header.vue";
 import HomePage from "./pages/HomePage.vue";
 import CityCard from "./components/partials/CityCard.vue";
+import Footer from './components/Footer.vue';
 export default {
   components: {
     Header,
     HomePage,
     CityCard,
+    Footer,
   },
 };
 </script>
@@ -15,7 +17,10 @@ export default {
 
   <Header/>
   <router-view></router-view>
-  
+  <Footer />
+
 </template>
 
-<style></style>
+<style>
+
+</style>
