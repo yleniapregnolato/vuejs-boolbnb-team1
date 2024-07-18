8f5387f5f1d2cdf901459818b31c3031259ff973
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-18 17:19:57 +0200

    feat: routs in ShowPage

--- src/pages/ShowPage.vue
+++ src/pages/ShowPage.vue
@@ -1,9 +1,52 @@
 <script>
+import axios from "axios";
+export default {
+    props: {
+            flats: Object,
+        },
+    data() {
+        return {
+            
+        };
+    },
+   
+}
 </script>
 
 <template>
+    <div class="show-container">
+        <div class="ms_showcontainer">
+            <!-- galleria immagini -->
+            <section>
 
+            </section>
+            <!-- /galleria immagini -->
+
+            <!-- info appartamento -->
+            <section>
+                <h1>{{ flat.title }}</h1>
+            </section>
+            <!-- /info appartamento -->
+
+            <!-- mappa -->
+            <section>
+
+            </section>
+            <!-- /mappa -->
+        </div>
+
+    </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.show-container {
+    padding: 1rem;
+
+    .ms_showcontainer {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        margin-top: 60px;
+    }
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // Importare percorsi file
 import HomePage from "./pages/HomePage.vue";
+import ShowPage from "./pages/ShowPage.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomePage,
+  },
+  {
+    path: "/show/:slug",
+    name: "show",
+    component: ShowPage,
   },
 ];
 
