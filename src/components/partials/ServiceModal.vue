db5eaf3cbf95db7a4ff23fe70709b42b5de1e37c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-19 15:44:14 +0200

    Revert "feat: ServiceModal.vue"
    This reverts commit 5c35c5fb5b38ef0b4a9cf3a6a07bce4298294e1d.

--- src/components/partials/ServiceModal.vue
+++ src/components/partials/ServiceModal.vue
@@ -11,16 +11,12 @@ export default {
     data() {
         return {
             showServices: false,
-            baseUrl: 'http://127.0.0.1:8000/storage/services',
         }
     },
     methods: {
         serviceModal() {
             this.showServices = !this.showServices;
             console.log(this.showServices);
-        },
-        getIconUrl(iconPath) {
-            return `${this.baseUrl}${iconPath}`;
         }
     }
 }
@@ -30,64 +26,34 @@ export default {
     <!-- modale servizi -->
 
     <!-- Modale servizi -->
-    <button @click="serviceModal" class="btn ms_brown_btn mt-4">Mostra tutti i servizi</button>
+    <button @click="serviceModal" class="btn btn-primary">Mostra servizi</button>
 
-    <div class="modal ms_service-modal" :class="showServices ? 'd-block' : ''" tabindex="-1">
+    <div class="modal ms_service-modal" :class="showServices ? 'd-block' : '' " tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Tutti i servizi</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
-                        @click="serviceModal"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="serviceModal"></button>
                 </div>
                 <div class="modal-body">
                     <ul>
-                        <li v-for="(service, index) in services" :key="index">
-                            <img :src="`${baseUrl}/${service.icon}`" alt="icon">
-                            <span class="ms-3">{{ service.name }}</span>
-                        </li>
+                        <li v-for="(service, index) in services" :key="index">{{ service.name }}</li>
                     </ul>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn ms_brown_btn" @click="serviceModal">Chiudi</button>
+                    <button type="button" class="btn btn-secondary" @click="serviceModal">Chiudi</button>
                 </div>
             </div>
         </div>
     </div>
+
 </template>
 
 <style lang="scss" scoped>
+
 .ms_service-modal {
     top: 30%;
 }
 
-.ms_brown_btn {
-    background-color: #705D3F;
-    border: 1px solid #705D3F;
-    color: white;
 
-    &:hover {
-        background-color: #F8F2EB;
-        transition: all 0.7s;
-        color: black;
-        border: 1px solid #F8F2EB;
-    }
-}
-
-ul {
-    list-style: none;
-
-    li {
-        color: #705D3F;
-        border: 1px solid #705D3F;
-        border-radius: 4px;
-        padding: 5px;
-        margin: 5px;
-        img {
-            width: 20px;
-            object-fit: cover;
-            filter: invert(24%) sepia(25%) saturate(356%) hue-rotate(7deg) brightness(89%) contrast(85%);
-        }
-    }
-}
 </style>
