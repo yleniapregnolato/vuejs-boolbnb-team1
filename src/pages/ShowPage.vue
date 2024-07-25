178da4e9a96454229376b73697989fc8d7531c7f
yleniapregnolato <yleniapregnolato@gmail.com>
2024-07-25 15:50:57 +0200

    fix: carousel in ShowPage.vue

--- src/pages/ShowPage.vue
+++ src/pages/ShowPage.vue
@@ -54,7 +54,7 @@ export default {
 
             <div class="row">
                 <div class="col-md-6 col-12">
-                    <img class="card-image img-fluid ms_photos h-100" :src="`${flatCoverImg}/${flat.main_img}`"
+                    <img class="card-image img-fluid ms_photos h-100 fixed-size" :src="`${flatCoverImg}/${flat.main_img}`"
                         alt="immagine di copertina" />
                 </div>
                 <!-- carosello -->
@@ -63,7 +63,7 @@ export default {
                         <div id="carouselExampleFade" class="carousel slide carousel-fade">
                             <div class="carousel-inner">
                                 <div class="carousel-item active" v-for="(photo, index) in photos.slice(0, 4)" :key="index">
-                                    <img class="ms_photo img-fluid card-image d-block h-100"
+                                    <img class="ms_photo img-fluid card-image d-block h-100 fixed-size"
                                         :src="`${flatPhotosUrl}/${photo.image}`" alt="immagini" />
                                 </div>
                             </div>
@@ -176,6 +176,11 @@ export default {
             color: black;
             border: 1px solid #F8F2EB;
         }
+    }
+    .fixed-size {
+        max-width: 100%;
+        max-height: 400px; /* Imposta l'altezza massima desiderata */
+        object-fit: cover;
     }
 }
 </style>
